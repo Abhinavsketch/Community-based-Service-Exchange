ea43a21151ccf454eb31f079d38986759e2f0054
Abhinavsketch <abhinavchaubey916@gmail.com>
2024-10-20 22:38:16 +0530

    Connecting Pages

---
+++ LOGIN HOME/home.js
@@ -0,0 +1,10 @@
+const loginsign = document.querySelector(".loginsign");
+const getstart = document.querySelector(".getstart");
+
+loginsign.addEventListener("click", () => {
+  window.location.href = "http://127.0.0.1:5500/LOGIN%20PAGE/login.html";
+});
+
+getstart.addEventListener("click", () => {
+  window.location.href = "http://127.0.0.1:5500/LOGIN%20PAGE/login.html";
+});
